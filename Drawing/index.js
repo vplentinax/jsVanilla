ab6f37762a6785cd4859bb479964cebb24cf4672
Valentiina VP <34555644+vplentinax@users.noreply.github.com>
2021-02-10 17:12:34 +0100

    Update index.js

--- Drawing/index.js
+++ Drawing/index.js
@@ -16,36 +16,36 @@ let lastY = 0
 let hue = 0
 
 function draw(e) {
-	if (!isDrawing) return
-	context.strokeStyle = `hsl(${hue++}, 100%, 50%)`
-	canvas.style.border = `5px solid hsl(${hue}, 100%, 50%)`
-	context.beginPath()
-	context.moveTo(lastX, lastY)
-	context.lineTo(e.offsetX, e.offsetY)
-	context.stroke()
-	;[lastX, lastY] = [e.offsetX, e.offsetY]
+  if (!isDrawing) return
+  context.strokeStyle = `hsl(${hue++}, 100%, 50%)`
+  canvas.style.border = `5px solid hsl(${hue}, 100%, 50%)`
+  context.beginPath()
+  context.moveTo(lastX, lastY)
+  context.lineTo(e.offsetX, e.offsetY)
+  context.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY]
 
-	if (hue >= 360) {
-		hue = 0
-	}
+  if (hue >= 360) {
+    hue = 0
+  }
 
-	if (context.lineWidth > 100 || context.lineWidth <= 1) {
-		direction = !direction
-	}
-	if (direction) {
-		context.lineWidth++
-	} else {
-		context.lineWidth--
-	}
+  if (context.lineWidth > 100 || context.lineWidth <= 1) {
+    direction = !direction
+  }
+  if (direction) {
+    context.lineWidth++
+  } else {
+    context.lineWidth--
+  }
 }
 
 canvas.addEventListener('mousemove', draw)
 canvas.addEventListener('mouseup', () => {
-	isDrawing = false
-	context.lineWidth = 1
+  isDrawing = false
+  context.lineWidth = 1
 })
 canvas.addEventListener('mouseout', () => (isDrawing = false))
 canvas.addEventListener('mousedown', e => {
-	isDrawing = true
-	;[lastX, lastY] = [e.offsetX, e.offsetY]
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY]
 })
