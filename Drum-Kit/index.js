8cd8de71a67185da455e13e214279ca752ba9088
Valentiina VP <34555644+vplentinax@users.noreply.github.com>
2021-02-10 17:11:36 +0100

    Update index.js

--- Drum-Kit/index.js
+++ Drum-Kit/index.js
@@ -1,16 +1,16 @@
 function removeTransition(e) {
-	if (e.propertyName !== 'transform') return
-	e.target.classList.remove('playing')
+  if (e.propertyName !== 'transform') return
+  e.target.classList.remove('playing')
 }
 
 function playSound(e) {
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-	const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
-	if (!audio) return
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+  if (!audio) return
 
-	key.classList.add('playing')
-	audio.currentTime = 0
-	audio.play()
+  key.classList.add('playing')
+  audio.currentTime = 0
+  audio.play()
 }
 
 const keys = Array.from(document.querySelectorAll('.key'))
