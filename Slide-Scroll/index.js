7d263664505a35566fd6045e1711198242a02c41
Valentiina VP <34555644+vplentinax@users.noreply.github.com>
2021-02-10 17:10:07 +0100

    Update index.js

--- Slide-Scroll/index.js
+++ Slide-Scroll/index.js
@@ -1,33 +1,34 @@
 function debounce(func, wait = 20, immediate = true) {
-	var timeout
-	return function () {
-		var context = this,
-			args = arguments
-		var later = function () {
-			timeout = null
-			if (!immediate) func.apply(context, args)
-		}
-		var callNow = immediate && !timeout
-		clearTimeout(timeout)
-		timeout = setTimeout(later, wait)
-		if (callNow) func.apply(context, args)
-	}
+  var timeout
+  return function() {
+    var context = this,
+      args = arguments
+    var later = function() {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
 }
 
 const sliderImages = document.querySelectorAll('.slide-in')
 
 function checkSlide(e) {
-	sliderImages.forEach(slideImage => {
-		const slideAt = window.scrollY + window.innerHeight - slideImage.height / 2
-		const imageBottom = slideImage.offsetTop + slideImage.height
-		const isHalfShown = slideAt > slideImage.offsetTop
-		const isNotScrollPast = window.scrollY < imageBottom
-		if (isHalfShown && isNotScrollPast) {
-			slideImage.classList.add('active')
-		} else {
-			slideImage.classList.remove('active')
-		}
-	})
+  sliderImages.forEach(slideImage => {
+    const slideAt = window.scrollY + window.innerHeight - slideImage
+      .height / 2
+    const imageBottom = slideImage.offsetTop + slideImage.height
+    const isHalfShown = slideAt > slideImage.offsetTop
+    const isNotScrollPast = window.scrollY < imageBottom
+    if (isHalfShown && isNotScrollPast) {
+      slideImage.classList.add('active')
+    } else {
+      slideImage.classList.remove('active')
+    }
+  })
 }
 
 window.addEventListener('scroll', debounce(checkSlide))
